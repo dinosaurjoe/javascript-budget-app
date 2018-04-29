e85cc66b74cf7cffdb4a75ba04a5da43fdd1f7e0
Joe Schafer <joeschaferastula@gmail.com>
2018-04-29 17:55:26 -0500

    set up architecture with controllers

---
+++ app.js
@@ -0,0 +1,15 @@
+var budgetController = (function() {
+
+})();
+
+
+var UIController = (function() {
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+
+})(budgetController, UIController);
