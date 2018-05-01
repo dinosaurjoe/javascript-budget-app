a218b2f798ac04af971e7b9eb11ebe8564e9c8e8
Joe Schafer <joeschaferastula@gmail.com>
2018-05-01 14:30:14 -0500

    clear input fields after entering data and disable add when data is invalid

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ var UIController = (function() {
               // description of what it is
               description: document.querySelector(DOMstrings.inputDescription).value,
               // numerical value of the income or expense
-              value: document.querySelector(DOMstrings.inputValue).value
+              value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -113,6 +113,23 @@ var UIController = (function() {
           // Insert the HTML into the DOM
           document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
+        },
+
+        clearFields: function() {
+          var fields, fieldsArr;
+
+          // Select the input description and the input value
+          fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+          // Call the input description into the array prototype
+          fieldsArr = Array.prototype.slice.call(fields);
+
+          // Iterate over the array and apply an anonymous function to the array to clear the input after it's been called
+          fieldsArr.forEach(function(current, index, array) {
+              current.value = "";
+          });
+
+          fieldsArr[0].focus();
         },
 
         getDOMstrings: function() {
@@ -142,17 +159,34 @@ var controller = (function(budgetCtrl, UICtrl) {
       });
     };
 
+    var updateBudget = function() {
+
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the user interface
+    };
+
+
     var ctrlAddItem = function() {
         var input, newItem;
         // 1. Get input field input data
         input = UIController.getInput();
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // 3. Add the new item to the user interface
-        UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget
 
-        // 5. Display the budget on the user interface
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3. Add the new item to the user interface
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+        }
+
     };
 
 
