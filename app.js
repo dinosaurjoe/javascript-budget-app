c0358e534ea477a04c745da2c8f6f2d5f9342855
Joe Schafer <joeschaferastula@gmail.com>
2018-05-03 14:07:49 -0500

    expenses turn form red

--- app.js
+++ app.js
@@ -191,6 +191,12 @@ var UIController = (function() {
       dateLabel: '.budget__title--month'
     };
 
+    var nodeListForEach = function(list, callback){
+              for (var i = 0; i < list.length; i++) {
+                callback(list[i], i);
+              }
+        };
+
     var formatNumber = function(num, type){
             var numSplit, int, dec, type;
             /*
@@ -293,12 +299,6 @@ var UIController = (function() {
 
           var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-          var nodeListForEach = function(list, callback){
-              for (var i = 0; i < list.length; i++) {
-                callback(list[i], i);
-              }
-          };
-
           nodeListForEach(fields, function(current, index) {
               if (percentages[index] > 0 ) {
                   current.textContent = percentages[index] + '%';
@@ -318,6 +318,21 @@ var UIController = (function() {
           year = now.getFullYear();
           month = now.getMonth();
           document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+              cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputButton).classList.toggle('red');
 
         },
 
@@ -348,6 +363,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       });
 
       document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+      document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
